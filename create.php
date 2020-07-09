c02b8f2a47f44a408766b99be243e296a4b70916
kittyjosh <joshua.shi@gmail.com>
2020-07-09 14:24:35 -0700

    show number of running processes

--- create.php
+++ create.php
@@ -1,4 +1,5 @@
 <?php
+$process_count = shell_exec('ps -e o pid,ppid,pgid,cmd | grep "orca " | grep -v grep | wc -l');
   if(!empty($_FILES['uploaded_file']))
   {
     $nonce = md5(time());
@@ -27,6 +28,8 @@
   <title>Upload your orca input file</title>
 </head>
 <body>
+  <h1>Upload your orca input file, provide a valid email address</h1>
+  <h2<?php if ($process_count>=3) echo " style=\"color:Tomato;\""; ?>>Running orca process count: <?php echo $process_count; ?></h2>
   <form enctype="multipart/form-data" action="create.php" method="POST">
     <h2>Upload your orca input file</h2>
     <p>Your email address (orca result will be emailed to you):
